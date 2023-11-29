e85645424bfd867620e2fc61a5e81eab298da1d9
hy <heyang.heyang@alibaba-inc.com>
2023-11-29 14:22:31 +0800

    Update dependencies in go.mod and initialize client with additional information

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
+	"runtime"
 	"syscall"
 
 	"github.com/judwhite/go-svc"
@@ -15,8 +18,55 @@ import (
 	_ "github.com/open-dingtalk/ipaas-net-gateway/logger"
 )
 
+func printWelcomePage() {
+	// 打印欢迎页面
+	fmt.Println("====================================")
+	fmt.Println("= Welcome to use ipaas-net-gateway =")
+	fmt.Println("= Version: 0.0.1                   =")
+	fmt.Println("====================================")
+}
+
 type program struct {
 	cli *client.StreamClient
+}
+
+func (p *program) initClient(env svc.Environment) error {
+	clientId := config.GetConfig().Cleint.ClientId
+	clientSecret := config.GetConfig().Cleint.ClientSecret
+
+	StreamClientLogger.SetLogger(StreamClientLogger.NewStdTestLogger())
+
+	extra := make(map[string]string)
+	// 获取操作系统版本
+	extra["osVersion"] = runtime.GOOS
+	// 获取主机名
+	hostname, err := os.Hostname()
+	if err != nil {
+		return err
+	}
+	extra["hostname"] = hostname
+
+	var openApiHost string
+	if runtime.GOOS == "windows" {
+		openApiHost = "https://windows-api.dingtalk.com"
+	} else {
+		openApiHost = "https://pre-api.dingtalk.com"
+	}
+
+	p.cli = client.NewStreamClient(
+		client.WithAppCredential(client.NewAppCredentialConfig(clientId, clientSecret)),
+		client.WithOpenApiHost(openApiHost),
+		client.WithExtras(extra))
+
+	//注册事件类型的处理函数
+	p.cli.RegisterCallbackRouter("/v1.0/ipaas/proxy/callback", func(c context.Context, df *payload.DataFrame) (*payload.DataFrameResponse, error) {
+		logger := zap.L()
+		logger.Info("receive data frame: ", zap.Any("data frame", df.Data))
+		response, _ := HandleIpaasCallBack(c, df)
+		return response, nil
+	})
+
+	return nil
 }
 
 func main() {
@@ -31,24 +81,12 @@ func (p *program) Init(env svc.Environment) error {
 		// windows service specific actions
 	}
 
-	clientId := config.GetConfig().Cleint.ClientId
-	clientSecret := config.GetConfig().Cleint.ClientSecret
-
-	StreamClientLogger.SetLogger(StreamClientLogger.NewStdTestLogger())
-	p.cli = client.NewStreamClient(client.WithAppCredential(client.NewAppCredentialConfig(clientId, clientSecret)))
-
-	//注册事件类型的处理函数
-	p.cli.RegisterCallbackRouter("/v1.0/ipaas/proxy/callback", func(c context.Context, df *payload.DataFrame) (*payload.DataFrameResponse, error) {
-		logger := zap.L()
-		logger.Info("receive data frame: ", zap.Any("data frame", df.Data))
-		response, _ := HandleIpaasCallBack(c, df)
-		return response, nil
-	})
-
-	return nil
+	return p.initClient(env)
 }
 
 func (p *program) Start() error {
+	printWelcomePage()
+
 	err := p.cli.Start(context.Background())
 	if err != nil {
 		return err
