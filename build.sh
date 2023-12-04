f6f3ced61fe9b343374ec1e3bdfb8250595d1295
hy <heyang.heyang@alibaba-inc.com>
2023-12-04 14:09:18 +0800

    Update build script to include version in executable names

--- build.sh
+++ build.sh
@@ -4,19 +4,19 @@
 mkdir -p bin
 
 # APP Version
-APP_VERSION=1.0.0
+APP_VERSION=0.0.1
 
 # APP Name
 APP_NAME=agent
 
 # 编译Windows的可执行文件
-GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$APP_VERSION" -o bin/${APP_NAME}_windows_amd64.exe
+GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$APP_VERSION" -o bin/${APP_NAME}_windows_amd64_${APP_VERSION}.exe
 
 # 编译Linux的可执行文件
-GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$APP_VERSION" -o bin/${APP_NAME}_linux_amd64
+GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$APP_VERSION" -o bin/${APP_NAME}_linux_amd64_${APP_VERSION}
 
 # 编译mac的可执行文件 (Intel)
-GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$APP_VERSION" -o bin/${APP_NAME}_darwin_amd64
+GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$APP_VERSION" -o bin/${APP_NAME}_darwin_amd64_${APP_VERSION}
 
 # 编译mac的可执行文件 (ARM)
-GOOS=darwin GOARCH=arm64 go build -ldflags "-X main.version=$APP_VERSION" -o bin/${APP_NAME}_darwin_arm64
+GOOS=darwin GOARCH=arm64 go build -ldflags "-X main.version=$APP_VERSION" -o bin/${APP_NAME}_darwin_arm64_${APP_VERSION}
